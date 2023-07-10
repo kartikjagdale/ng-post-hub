6248b090cf5796f2a9f65c4bf49c295d37507492
voidzero <kartikjagdale11@gmail.com>
2023-07-10 19:59:21 +0530

    add test cases for app component and posts service

--- apps/posts-app/src/app/app.component.spec.ts
+++ apps/posts-app/src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, NxWelcomeComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -17,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'posts-app'`, () => {
+  it(`should have as title 'NgPostHub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('posts-app');
+    expect(app.title).toEqual('NgPostHub');
   });
 
   it('should render title', () => {
@@ -28,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome posts-app'
+      'NgPostHub'
     );
   });
 });
--- libs/posts/data-access/src/lib/services/posts.service.spec.ts
+++ libs/posts/data-access/src/lib/services/posts.service.spec.ts
@@ -1,16 +1,69 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { PostsService } from './posts.service';
+import { PostsEntity } from '../+state/posts.models';
 
 describe('PostsService', () => {
   let service: PostsService;
+  let httpMock: HttpTestingController;
+  const jsonPostsAPIUrl = 'https://jsonplaceholder.typicode.com/posts';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PostsService]
+    });
+
     service = TestBed.inject(PostsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('Success Scenarios', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    it('should have as title "PostHub"', () => {
+      expect(service.title).toEqual('PostHub');
+    });
+
+    it('should fetch all posts', () => {
+      const dummyPosts: PostsEntity[] = [
+        { id: 1, title: 'Test Post 1', body: 'Test Body 1', userId: 1 },
+        { id: 2, title: 'Test Post 2', body: 'Test Body 2', userId: 2 },
+      ];
+
+      service.getAllPosts().subscribe(posts => {
+        expect(posts.length).toBe(2);
+        expect(posts).toEqual(dummyPosts);
+      });
+
+      const req = httpMock.expectOne(jsonPostsAPIUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyPosts);
+    });
+
+  });
+
+  describe('Error Scenarios', () => {
+    it('should handle http error safely', () => {
+      const errorMessage = 'Dummy error message';
+
+      service.getAllPosts().subscribe(
+        _ => fail('should have failed with an error message'),
+        (error: string) => {
+          expect(error).toEqual(errorMessage);
+        }
+      );
+
+      const req = httpMock.expectOne(jsonPostsAPIUrl);
+
+      // Respond with mock error
+      req.error(new ErrorEvent(errorMessage));
+    });
   });
 });
